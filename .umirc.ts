438ad75b8fe0e1daea0f8dfddcfcd15506341c36
shilili1 <shilili1@jd.com>
2022-06-24 16:29:29 +0800

    feat: 修改名称

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'dumi';
-
-export default defineConfig({
-  title: 'easy-hooks',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
+export default {
+  base: '/easy-hooks', // 文档起始路由
   publicPath: '/easy-hooks/', // 静态资源起始路径
-  // more config: https://d.umijs.org/config
-});
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  // 其他配置
+};
